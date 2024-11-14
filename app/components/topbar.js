4de4db5f9f49fca8f85771091ff24ce981678a8b
SAWKIT <potions.plaquiste0l@icloud.com>
2024-11-15 00:09:51 +0100

    Clean code

--- app/components/topbar.js
+++ app/components/topbar.js
@@ -4,11 +4,11 @@ function Topbar() {
     return (
         <div className="topbar flex flex-row w-full py-3 px-4 justify-between">
             <a className="flex items-center">
-                <h1>Nicolas Wieckiewicz</h1>
+                <h1><span>Nicolas</span> <span className="uppercase">Wieckiewicz</span></h1>
             </a>
             <nav className="font-firacode-medium">
-                <div className="flex flex-col justify-center mr-3">
-                    <ul className="flex flex-row space-x-3 justify-center mr-3">
+                <div>
+                    <ul className="flex flex-row space-x-3 justify-center">
                         <li>
                             <span>/</span>
                             <span className="ml-3">Info</span>
