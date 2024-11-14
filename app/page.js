6fe877263fddfabcb4bbd0ecf635e26c6f40cdbc
SAWKIT <potions.plaquiste0l@icloud.com>
2024-11-15 00:00:24 +0100

    HomePage created

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Topbar from "./components/topbar";
+import Home from "./components/home";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-[100dvh] font-luciole">
       <Topbar />
-      <main className="flex flex-col justify-center h-full">
-        <div className="flex justify-center pt-4">
-          <img className="w-40" src="/homer.png"></img>
+      <main className="flex flex-col justify-center h-full bg-white">
+        <div className="flex justify-center pt-4 m-4 h-full border-4 border-slate-800">
+          <img className="w-40 h-min" src="/homer.png"></img>
         </div>
       </main>
       <footer className="">
