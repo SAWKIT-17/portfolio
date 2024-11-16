d78be456d76650011f6bc4fa911fe9d32c0617d4
SAWKIT <potions.plaquiste0l@icloud.com>
2024-11-16 01:00:47 +0100

    Info deleted

--- app/components/home.js
+++ app/components/home.js
@@ -2,17 +2,13 @@ import React from "react";
 
 function Homepage() {
     return (
-        <div className="">
+        <div>
             <div className="flex flex-row">
                 <img className="w-40 h-min rounded-full" src="/nicolas_w.jpg"></img>
                 <div className="ml-4">
                     <div className="flex flex-row align-middle">
                         <img className="w-6 h-6" src="/icons/person.svg" />
                         <h1 className=""><span>Nicolas</span> <span>Wieckiewicz</span></h1>
-                    </div>
-                    <div className="flex flex-row align-middle">
-                        <img className="w-6 h-6" src="/icons/cake.svg" />
-                        <h1 className="">21/04/1997</h1>
                     </div>
                 </div>
             </div>
