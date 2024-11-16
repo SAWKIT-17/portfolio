33a8de756d47ef595afab3888f257ba4ffe190f2
SAWKIT <potions.plaquiste0l@icloud.com>
2024-11-16 01:34:10 +0100

    Summary created

--- app/components/home.js
+++ app/components/home.js
@@ -3,12 +3,22 @@ import React from "react";
 function Homepage() {
     return (
         <div>
+
             <div className="flex flex-row">
                 <img className="w-40 h-min rounded-full" src="/nicolas_w.jpg"></img>
                 <div className="ml-4">
-                    <div className="flex flex-row align-middle">
-                        <img className="w-6 h-6" src="/icons/person.svg" />
-                        <h1 className=""><span>Nicolas</span> <span>Wieckiewicz</span></h1>
+                    <div className="flex flex-col align-middle">
+                        <h1 className="">Développeur Full-Stack</h1>
+                        <h2 className=""><span>Nicolas</span> <span>Wieckiewicz</span></h2>
+                        <hr></hr>
+                        <span>
+                            Je suis un développeur full-stack passionné, avec une solide formation en IT & Réseaux. 
+                            <br />
+                            Après avoir travaillé pour des organisations prestigieuses comme la Commission européenne via Bryton Global et occupé un rôle de développeur et gestionnaire de projet chez Tafsquare, j'ai décidé de me lancer en tant que freelance. 
+                            Polyvalent et autodidacte, j'explore des domaines variés tels que la programmation, l'IT, et les réseaux, avec une ambition de me spécialiser dans la cybersécurité et l'IA.
+                            <br />
+                            Je suis à votre service pour concrétiser vos projets web et IT avec professionnalisme et créativité.
+                        </span>
                     </div>
                 </div>
             </div>
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex flex-col h-[100dvh] font-luciole overflow-clip">
       <Topbar />
       <main className="flex flex-col justify-center h-screen overflow-hidden">
-        <div className="flex justify-center py-4 px-8 mx-4 mb-4 h-full max-h-full bg-white text-black border-4 border-blue-600 border-opacity-100 overflow-y-scroll">
+        <div className="flex justify-center py-4 px-8 mx-20 mb-6 h-full max-h-full bg-white text-black border-4 border-blue-600 border-opacity-100 overflow-y-scroll">
           <Homepage />
         </div>
       </main>
