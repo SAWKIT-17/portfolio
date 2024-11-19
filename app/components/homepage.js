d796f48b124e4ce680858bd409d8b57db074a8fa
SAWKIT <potions.plaquiste0l@icloud.com>
2024-11-19 02:32:06 +0100

    Homepage updated

--- app/components/homepage.js
+++ app/components/homepage.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Homepage() {
     return (
-        <div>
-            <span>Besoin d'un développeur ?</span>
-            <span>Pensez Nicolas Wieckiewicz</span>
+        <div className="w-screen h-full flex flex-col justify-center text-center lg:text-start">
+            <span className="mb-4 text-4xl bg-gradient-to-l bg-clip-text text-transparent from-violet-700 to-blue-300">Besoin d'un développeur ?</span>
+            <span className="text-6xl">Nicolas Wieckiewicz</span>
         </div>
     );
 }
